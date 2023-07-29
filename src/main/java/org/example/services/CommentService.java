0ea6cd460a9a106ead582f97d3f455c8cd5b4246
asrytikov <asrytikov@gmail.com>
2023-07-29 21:08:32 +0400

    Project18_2

--- src/main/java/org/example/services/CommentService.java
+++ src/main/java/org/example/services/CommentService.java
@@ -1,13 +1,18 @@
-package org.example;
+package org.example.services;
+
+import org.example.Comment;
+import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
 
+@Service
 public class CommentService {
 
     private Logger logger = Logger.getLogger(CommentService.class.getName());
 
-    public void publishComment(Comment comment){
+    public String publishComment(Comment comment){
         logger.info("Publishing comment:" + comment.getText());
+        return "SUCCESS";
     }
 
 }
